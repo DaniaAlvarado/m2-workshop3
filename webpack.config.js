6650c7e95d20e4e8bf563366ffd0e164fdb874b8
DaniaAlvarado <dania26alvarado@gmail.com>
2022-10-10 09:00:03 -0500

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "/src/index.js", // main js
+  output: {
+    path: path.resolve(__dirname, "build"), // output folder
+    publicPath: "/",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader,
+          "style-loader",
+          "css-loader", // for styles
+        ],
+      },
+      {
+        test: /\.scss$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html", // base html
+    }),
+    new MiniCssExtractPlugin({
+      filename: "css/app.bundle.css",
+    }),
+  ],
+};
